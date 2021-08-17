60c5eee88b1e31f827e15342d1db400a718edcd4
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2021-08-17 12:58:44 +0200

    Initial commit

---
+++ src/main/java/com/example/pizzagateway/PizzaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizzagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaGatewayApplication.class, args);
+    }
+
+}
