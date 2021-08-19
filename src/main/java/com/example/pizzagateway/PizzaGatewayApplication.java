b35cb5ca8a5ddb2dd97d8259b936bf9fd6efc8bd
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2021-08-19 10:12:56 +0200

    added basic auth with user and admin.

--- src/main/java/com/example/pizzagateway/PizzaGatewayApplication.java
+++ src/main/java/com/example/pizzagateway/PizzaGatewayApplication.java
@@ -2,6 +2,13 @@ package com.example.pizzagateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 public class PizzaGatewayApplication {
---
+++ src/main/java/com/example/pizzagateway/basicauth/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+package com.example.pizzagateway.basicauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService(){
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}password")
+                .roles("ADMIN")
+                .build();
+        return new MapReactiveUserDetailsService(user,admin);
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic()
+                .and()
+                .authorizeExchange()
+                .pathMatchers(HttpMethod.GET, "/pizzas/**").permitAll()
+                .anyExchange().hasRole("ADMIN")
+                .and()
+                .build();
+    }
+}
